0f551a81d2171e0ee2c94b60189e6822d3f6ee59
S.R. Shuvo <shuvo22890@gmail.com>
2024-12-22 17:57:02 +0600

    implemented jwt creating feature

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ynkon.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -14,9 +17,12 @@ const client = new MongoClient(uri, {
 });
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 app.use(express.json());
-
+app.use(cookieParser());
 
 async function run() {
     try {
@@ -36,6 +42,18 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result);
         });
+
+        // Create JWT after sign in
+        app.post('/jwt', (req, res)=>{
+            const user = req.body;
+            const token = jwt.sign(user, process.env.JWT_SECRET, {expiresIn: '5h'});
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite:  process.env.NODE_ENV === 'production' ? 'none' : 'strict'
+            }).send({success: true})
+        })
+
 
 
     } finally {
